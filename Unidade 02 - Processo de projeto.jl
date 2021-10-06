fde7dc9f29c49d0605adef60af6d748edd955a67
jefferds <jefferds@gmail.com>
2021-10-06 20:55:48 +0000

    Deploying to gh-pages from  @ 1ebffd1cf82b13725e2dcc27b3a50618241dff3a 🚀

---
+++ Unidade 02 - Processo de projeto.jl
@@ -0,0 +1,51 @@
+### A Pluto.jl notebook ###
+# v0.15.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e9af4550-b632-11eb-1d0d-e11b45468706
+html"<button onclick=present()> Presente </button>"
+
+# ╔═╡ 4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+md"""
+## Plano Unidade 02 - PROCESSO DE PROJETO 
+
+### Tema relacionado na ementa:
+
+2. Introdução ao desenho técnico mecânico auxiliado por computador (CAD). 
+
+**Fonte:** 1. GIESECKE, Frederick Ernest et al. _**Technical drawing**_. Macmillan, 2016..
+
+### Resultados de aprendizagem esperados
+
+- 🥇 Conhecer os fundamentos para PROJETO CAD.
+
+- 🥇 Desenvolver estratégias para elaboração de modelos em 3D. 
+
+- 🥇 Estabelecer relações interpessoais e trabalhar em equipe, comunicar-se oralmente e ter responsabilidade coletiva.
+"""
+
+# ╔═╡ 64cb0563-2802-459e-8b14-2430546554ab
+md"""
+### Roteiro
+
+🕐 1. Indicação do tema da atividade a ser executada por cada grupo *(até 05/0ut/2021)*,
+ 
+- _Processo de projeto utilizando CAD._;
+
+🕑 2. Estudar slides disponibilizados e buscar na rede outros conteúdos. Postar no Classroom resumo de até uma página. *(até 13/out/2021)*;
+
+🕔 3. Seleção de projeto a ser apresentado à turma *(até 13/out/2021)*;
+
+🕙 4. Entrega relatório projeto no classroom e apresentações *(até 14/out/2021)*;
+
+🕑 5. Avaliações ($(html"<a href='' target='_blank'>Formulário Eletrônico no Google Forms (Acesse aqui)</a>")) *(até 20/out/2021)* e
+
+🕒 6. Feedback (sala) *(até 20/out/2021)*.
+"""
+
+# ╔═╡ Cell order:
+# ╟─e9af4550-b632-11eb-1d0d-e11b45468706
+# ╟─4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+# ╟─64cb0563-2802-459e-8b14-2430546554ab
