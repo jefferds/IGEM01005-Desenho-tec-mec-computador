4738221f63cfabdf831e76b8589a9ace8bdff1c6
jefferds <jefferds@gmail.com>
2021-10-28 19:16:59 +0000

    Deploying to gh-pages from  @ f70cdfc54283216d5101cd2fef773321d935bc80 🚀

---
+++ Unidade 03 - Ferramenta de combinar (Loft).jl
@@ -0,0 +1,51 @@
+### A Pluto.jl notebook ###
+# v0.15.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e9af4550-b632-11eb-1d0d-e11b45468706
+html"<button onclick=present()> Presente </button>"
+
+# ╔═╡ 4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+md"""
+## Plano Unidade 03 - FERRAMENTA COMBINAR (LOFT)
+
+### Tema relacionado na ementa:
+
+4. Utilização de softwares.
+
+**Fonte:** 1. GIESECKE, Frederick Ernest et al. _**Technical drawing**_. Macmillan, 2016..
+
+### Resultados de aprendizagem esperados
+
+- 🥇 Conhecer a ferramenta de combinar do SpaceClaim.
+
+- 🥇 Adquirir prática de desenho utilizando SpaceClaim.
+
+- 🥇 Desenvolver estratégias para elaboração de modelos em 3D. 
+
+- 🥇 Estabelecer relações interpessoais e trabalhar em equipe, comunicar-se oralmente e ter responsabilidade coletiva.
+"""
+
+# ╔═╡ 64cb0563-2802-459e-8b14-2430546554ab
+md"""
+### Roteiro
+
+🕐 1. Indicação do tema da atividade a ser executada por cada grupo *(até 28/0ut/2021)*,
+ 
+- _FERRAMENTA COMBINAR (LOFT)_;
+
+🕑 2. Construir modelo 3d para impressão à partir do arquivo STL gerado na otimização topológica realizada no ANSYS. *(até 03/nov/2021)*;
+
+🕙 4. Entrega do modelo construído (aquivo zipado) e apresentações *(até 04/out/2021)*;
+
+🕑 5. Avaliações *(até 10/out/2021)* e
+
+🕒 6. Feedback (sala) *(até 10/out/2021)*.
+"""
+
+# ╔═╡ Cell order:
+# ╟─e9af4550-b632-11eb-1d0d-e11b45468706
+# ╟─4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+# ╟─64cb0563-2802-459e-8b14-2430546554ab
