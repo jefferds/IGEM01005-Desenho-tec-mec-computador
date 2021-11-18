8cce3a2f35c391d7159a6c8ebb4781cf4a96ce06
jefferds <jefferds@gmail.com>
2021-11-18 20:15:31 +0000

    Deploying to gh-pages from  @ 1efb29cfc0d0fc085b9302835e856e4e7549703e 🚀

---
+++ Unidade 04 - Desenho detalhado de uma estrutura de suporte.jl
@@ -0,0 +1,56 @@
+### A Pluto.jl notebook ###
+# v0.15.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e9af4550-b632-11eb-1d0d-e11b45468706
+html"<button onclick=present()> Presente </button>"
+
+# ╔═╡ 4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+md"""
+## Plano Unidade 04 - Desenho detalhado de uma estrutura de suporte
+
+### Tema relacionado na ementa:
+
+9. Desenho de peças, de elementos e de montagens de máquinas.
+
+**Fonte:** 1. GIESECKE, Frederick Ernest et al. _**Technical drawing**_. Macmillan, 2016..
+
+### Resultados de aprendizagem esperados
+
+- 🥇 Conhecer a ferramenta de Montagem de conjuntos (Máquinas e dispositivos).
+
+- 🥇 Adquirir prática de desenho utilizando SpaceClaim.
+
+- 🥇 Desenvolver estratégias para elaboração de modelos em 3D. 
+
+- 🥇 Estabelecer relações interpessoais e trabalhar em equipe, comunicar-se oralmente e ter responsabilidade coletiva.
+"""
+
+# ╔═╡ 64cb0563-2802-459e-8b14-2430546554ab
+md"""
+### Roteiro
+
+🕐 1. Indicação do tema da atividade a ser executada por cada grupo *(até 18/nov/2021)*,
+ 
+- _Desenho detalhado de uma estrutura_;
+
+🕑 2. Construir modelo da estrutura para uma peneira de areia.  *(até 25/nov/2021)*;
+
+- A estrutura deve ser projetada para ser desmontada, possuindo assim 4 partes.
+- Deve ser construida em perfis metálicos disponíveis no mercado.
+- A estrutura deve suportar uma peneira de 150 cm por 50 cm.
+- devem ser entregues os aquivos 3D e as folhas de desenho com o detalhamento (dimensões e outras informações) de cada uma da partes.
+
+🕙 3. Entrega dos arquivos dos modelos (aquivo zipados) e apresentações *(até 25/nov/2021)*;
+
+🕑 4. Avaliações *(até 02/dez/2021)* e
+
+🕒 5. Feedback (sala) *(até 02/dez/2021)*.
+"""
+
+# ╔═╡ Cell order:
+# ╟─e9af4550-b632-11eb-1d0d-e11b45468706
+# ╟─4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+# ╟─64cb0563-2802-459e-8b14-2430546554ab
